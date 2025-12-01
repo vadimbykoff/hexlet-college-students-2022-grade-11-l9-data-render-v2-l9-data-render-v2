7f9453aa2480042ca3bf84a0ee48db379a32ff41
Вадим Быков <144332762+vadimbykoff@users.noreply.github.com>
2025-12-01 10:56:14 +0300

    Update FilterCafes.jsx

--- src/FilterCafes.jsx
+++ src/FilterCafes.jsx
@@ -1,1 +1,47 @@
+import React from 'react';
 
+const FilterCafes = ({ onFilterChange }) => {
+  const subwayOptions = [
+    {
+      name: "Арбатская",
+      code: "Arbat",
+    },
+    {
+      name: "Александровский сад",
+      code: "Alexanders Garden",
+    },
+    {
+      name: "Московская",
+      code: "Moscow",
+    },
+    {
+      name: "Парк Культуры",
+      code: "Culture",
+    },
+    {
+      name: "Театральная",
+      code: "Theatr",
+    },
+  ];
+
+  const handleChange = (e) => {
+    if (onFilterChange) {
+      onFilterChange(e.target.value);
+    }
+  };
+
+  return (
+    <div className="controls">
+      <select name="subway" id="subway" onChange={handleChange}>
+        <option value="All">Все</option>
+        {subwayOptions.map((option) => (
+          <option key={option.code} value={option.code}>
+            {option.name}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default FilterCafes;
